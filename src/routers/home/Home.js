c57a3694a7818209decf47fef076e3e4025f60b9
obidxonn <obidxonoloviddinov1000@gmail.com>
2023-04-04 15:01:14 +0500

    some changes

--- src/routers/home/Home.js
+++ src/routers/home/Home.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import banner from '../../assets/banner.jpg'
+import videoImg from '../../assets/videoBackground.jpg'
+import ReactPlayer from 'react-player/youtube'
+import { AiOutlinePlayCircle } from 'react-icons/ai'
 import './Home.css'
 
 function Home() {
+  const [videoPlay, setVideoPlay] = useState(false)
+  const [play, setPlay] = useState(true)
+
+  function isPLaying() {
+    setPlay(false)
+    setVideoPlay(false)
+  }
+
   return (
     <div className='home__wrapper'>
       <div className="main__banner">
@@ -24,15 +35,19 @@ function Home() {
       </div>
       <div className="banner__story">
         <div className="second__banner container">
-            <div className="video">
-              <video url="https://www.youtube.com/watch?time_continue=3&v=dCVEY88Fn1k&embeds_euri=https%3A%2F%2Fdemo.w3layouts.com%2F&source_ve_path=MjM4NTE&feature=emb_title&ab_channel=CookingShooking">salom</video>
+          <div className="video__container">
+            <AiOutlinePlayCircle className='video__play__icon' onClick={() => setVideoPlay(true)} />
+            <img src={videoImg} alt="" />
+            <div  className="video__player" onClick={() => isPLaying()}  style={videoPlay ? { display: 'block' } : { display: "none" }}>
+              <ReactPlayer playing={play} className="video" url="https://www.youtube.com/watch?time_continue=3&v=dCVEY88Fn1k&embeds_euri=https%3A%2F%2Fdemo.w3layouts.com%2F&source_ve_path=MjM4NTE&feature=emb_title&ab_channel=CookingShooking" />
             </div>
-            <div className="second__banner__data">
-              <h5>Our Story</h5>
-              <h3>Burgers! You won't Find Anywhere Else with Best Quality <span>Ingredients</span></h3>
-              <p>Lorem ipsum viverra feugiat. Pellen tesque libero ut justo, ultrices in ligula. Semper at tempufddfel. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quae, consequatur voluptatem ad.</p>
-              <Link to="/">Read More</Link>
-            </div>
+          </div>
+          <div className="second__banner__data">
+            <h5>Our Story</h5>
+            <h3>Burgers! You won't Find Anywhere Else with Best Quality <span>Ingredients</span></h3>
+            <p>Lorem ipsum viverra feugiat. Pellen tesque libero ut justo, ultrices in ligula. Semper at tempufddfel. Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quae, consequatur voluptatem ad.</p>
+            <div><Link to="/" className='link'>Read More</Link></div>
+          </div>
         </div>
       </div>
     </div>
